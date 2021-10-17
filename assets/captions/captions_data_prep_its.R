25919fff2d4a37b176be5e5d5977395d7c809e30
jarrodscott <jarrod.jude.scott@gmail.com>
2021-10-16 19:45:40 -0500

    workflow version 1.0

---
+++ assets/captions/captions_data_prep_its.R
@@ -0,0 +1,9 @@
+## Table captions
+caption_tab("seq_summary_its", "Sample summary table including read changes at start and end of DADA2 workflow.")
+caption_tab("seq_summary_final_its", "Sample summary table showing the final number of reads and ASVs (per sample) as well as the number of reads/ASVs lost during curation.")
+#<small>`r caption_tab("qual_scores_after_its")`</small>
+
+## Figure captions
+caption_fig("qual_scores_its", "Aggregated quality score plots for forward (left) & reverse (right) reads.")
+#<small>`r caption_fig("read_length_its")`</small>
+
